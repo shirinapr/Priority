8939db54b8117df526e7a6d366955903a94c5e9a
shirinapr <shirinapr1@gmail.com>
2023-02-23 00:12:32 +0330

    change: welcome page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/globals.css';
+import '../core/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import store from 'store';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,20 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h3>
-        WELOCOME TO
-        <b> PRIORITY</b>
-      </h3>
+    <div
+      className="grid place-items-center h-screen
+     bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))]
+      from-gray-900 to-gray-600"
+    >
+      <div className="border-2 border-slate-800 rounded-[24px] absolute top-6 bottom-6 left-6 right-6 grid place-items-center">
+        <div
+          className="text-[120px] animate-text font-thin
+    bg-gradient-to-r from-gray-800 via-slate-900 to-slate-800 bg-clip-text
+     text-transparent"
+        >
+          <h3>WELCOME</h3>
+        </div>
+      </div>
     </div>
   );
 };
